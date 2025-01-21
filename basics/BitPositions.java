85af1b4616b3fef6faa1b677fac7099ac390cea6
Your Name <b_alaidar@kbtu.kz>
2025-01-21 12:12:17 +0500

    push some commit

--- basics/BitPositions.java
+++ basics/BitPositions.java
@@ -4,18 +4,30 @@ import java.util.Scanner;
 
 public class BitPositions {
     public static void main(String[] args) {
+        // Test the function with the example input
+        String input = "11001010";
+        System.out.println("Input:  " + input);
+        System.out.println("Output: " + swapBits(input));
+    }
 
-        String bytes = "10110011";
-        System.out.println(bytes);
-        int byteValue = Integer.parseInt(bytes, 2);
-        int firstBit = (byteValue >> 7) & 1;
+    public static String swapBits(String binaryString) {
+        byte inputByte = (byte) Integer.parseInt(binaryString, 2);
 
-        System.out.println(firstBit);
+        byte firstTwoBits = (byte) ((inputByte & 0b11000000) >>> 6);
 
-//        System.out.println(~(firstBit << 7));
-        byteValue &= ~(1 << 7);
-        String result = String.format("%8s", Integer.toBinaryString(byteValue)).replace(' ', '0');
-        System.out.println(result);
+        byte lastTwoBits = (byte) (inputByte & 0b00000011);
 
+        byte middleBits = (byte) (inputByte & 0b00111100);
+
+        byte result = (byte) (
+                (lastTwoBits << 6) |
+                        middleBits |
+                        firstTwoBits
+        );
+
+        String resultStr = String.format("%8s", Integer.toBinaryString(result & 0xFF))
+                .replace(' ', '0');
+
+        return resultStr;
     }
 }
