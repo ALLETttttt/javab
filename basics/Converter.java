658868b4a76794c6110877cb0265e48ee6b9c976
Bakdaulet <b_alaidar@kbtu.kz>
2025-01-21 18:26:06 +0500

    test number two

--- basics/Converter.java
+++ basics/Converter.java
@@ -11,7 +11,6 @@ public class Converter {
     public static void main(String[] args) {
         boolean conversionFlag = true;
 
-
         Scanner scanner = new Scanner(System.in);
 
         while (conversionFlag) {
