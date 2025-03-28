2c2133a7886604eeb7cb77e229e69e1b02fac25b
ALLETttttt <b_alaidar@kbtu.kz>
2025-03-28 20:41:15 +0500

    added synchronized block

---
+++ multithreading/synchronized_examples/sync_ex1/SynchronizedExchanger.java
@@ -0,0 +1,25 @@
+package multithreading.synchronized_examples.sync_ex1;
+
+public class SynchronizedExchanger {
+    private Object object = null;
+
+    public synchronized Object getObject() {
+        return object;
+    }
+
+    public synchronized void setObject(Object object) {
+        this.object = object;
+    }
+
+    public Object getObj() {
+        synchronized (this) {
+            return object;
+        }
+    }
+
+    public void setObj(Object object) {
+        synchronized (this) {
+            this.object = object;
+        }
+    }
+}
